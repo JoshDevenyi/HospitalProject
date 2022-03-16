09cbbb050b925de35be629ad0a5bc10fb40a8af9
Josh <joshdevenyi@gmail.com>
2022-03-16 09:15:22 -0400

    Added test controller

---
+++ HospitalProject/Models/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject.Models
+{
+    public class test
+    {
+        //HI DYLAN!!!
+    }
+}
